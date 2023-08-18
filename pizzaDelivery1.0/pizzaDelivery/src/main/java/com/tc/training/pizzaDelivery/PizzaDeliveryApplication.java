fc8202a92d187d16aeb1909e267b5d9bfb939f4d
Namratha N <namratha.n@thoughtclan.com>
2023-08-18 12:02:45 +0530

    Trial push

---
+++ pizzaDelivery1.0/pizzaDelivery/src/main/java/com/tc/training/pizzaDelivery/PizzaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.tc.training.pizzaDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaDeliveryApplication.class, args);
+	}
+
+}
---
+++ pizzaDelivery1.0/pizzaDelivery/src/test/java/com/tc/training/pizzaDelivery/PizzaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.training.pizzaDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
